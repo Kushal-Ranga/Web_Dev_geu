843fb8b741cea1a50a6c0fa386ce7754b38b3f14
Kushal Ranga <44035280+Kushal-Ranga@users.noreply.github.com>
2021-02-01 21:13:04 +0530

    first html/php code

---
+++ hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML DEMO</title>
+</head>
+<body>
+<h1>Welcome Everyone</h1>
+<h2>Welcome Everyone</h2>
+<h3>Welcome Everyone</h3>
+<h4>Welcome Everyone</h4>
+<h5>Welcome Everyone</h5>
+<h6>Welcome Everyone</h6>
+
+
+
+</body>
+</html>
